075a4c796b6a5a39a98fed7c0bbfd0d7adc853c8
Karl McPhee <karmcphee@cox.net>
2024-01-02 12:03:42 -0500

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by Karl McPhee on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
